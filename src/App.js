51d77ce39addc748f3d129e98821c4673eb5d6c3
Swarup Das <swarupd049@gmail.com>
2023-11-12 12:29:44 +0530

    created qa branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
     <h1 className="text-3xl font-bold underline text-center">
-      Welcome to Unity6
+      Welcome to Unity6 qa branch
     </h1>
     </div>
   );
